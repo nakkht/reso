c0b58fd43fa82e17087693422d6e05e80fefa6d6
Paulius Gudonis <pg@neqsoft.com>
2019-06-25 00:05:18 +0200

    Implemented base InfoActivity Added base theming

---
+++ core/src/main/java/com/neqsoft/reso/info/InfoActivity.java
@@ -0,0 +1,17 @@
+package com.neqsoft.reso.info;
+
+import android.os.Bundle;
+
+import com.neqsoft.reso.R;
+
+import androidx.annotation.Nullable;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class InfoActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.activity_info);
+  }
+}
